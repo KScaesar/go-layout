87def8bb571a21ea434578db571e61aa74ad187c
ksCaesar <x246libra@hotmail.com>
2024-09-10 11:58:14 +0800

    add service release

--- pkg/service.go
+++ pkg/service.go
@@ -7,7 +7,8 @@ import (
 )
 
 var (
-	commit string
+	commit  string
+	release string
 )
 
 var defaultService atomic.Pointer[service]
@@ -18,30 +19,39 @@ func Service() service {
 
 func newService(name string) *service {
 	Commit := commit
+	goVersion := ""
 	if Commit == "" {
 		if info, ok := debug.ReadBuildInfo(); ok {
+			goVersion = info.GoVersion
+
 			for _, setting := range info.Settings {
 				if setting.Key == "vcs.revision" {
-					Commit = setting.Value
+					Commit = setting.Value[:8]
 				}
 			}
 		}
 	}
 
 	return &service{
-		Name:   name,
-		Commit: Commit,
+		Name:      name,
+		Commit:    Commit,
+		Release:   release,
+		GoVersion: goVersion,
 	}
 }
 
 type service struct {
-	Name   string `json:"name"`
-	Commit string `json:"commit"`
+	Name      string `json:"name"`
+	Commit    string `json:"commit"`
+	Release   string `json:"release"`
+	GoVersion string `json:"go_version"`
 }
 
 func (svc service) LogValue() slog.Value {
 	return slog.GroupValue(
 		slog.String("name", svc.Name),
 		slog.String("commit", svc.Commit),
+		slog.String("release", svc.Release),
+		slog.String("go_version", svc.GoVersion),
 	)
 }
