8305fd8e079562baac9bb7d685188360523ba386
ksCaesar <x246libra@hotmail.com>
2024-10-24 17:27:56 +0800

    refactor: BiMap

--- pkg/utility/bi_map.go
+++ pkg/utility/bi_map.go
@@ -2,19 +2,25 @@ package utility
 
 import (
 	"fmt"
+	"unique"
 )
 
 func NewBiMap[K, V comparable]() *BiMap[K, V] {
 	return &BiMap[K, V]{
-		keyMap: make(map[K]V),
-		valMap: make(map[V]K),
+		keyMap: make(map[unique.Handle[K]]unique.Handle[V]),
+		valMap: make(map[unique.Handle[V]]unique.Handle[K]),
+		keys:   make(map[K]V),
+		values: make(map[V]K),
 	}
 }
 
 // BiMap represents a one-to-one bidirectional map
 type BiMap[K, V comparable] struct {
-	keyMap map[K]V
-	valMap map[V]K
+	keyMap map[unique.Handle[K]]unique.Handle[V]
+	valMap map[unique.Handle[V]]unique.Handle[K]
+
+	keys   map[K]V
+	values map[V]K
 }
 
 func (b *BiMap[K, V]) MustSet(key K, val V) *BiMap[K, V] {
@@ -26,28 +32,37 @@ func (b *BiMap[K, V]) MustSet(key K, val V) *BiMap[K, V] {
 }
 
 func (b *BiMap[K, V]) Set(key K, val V) (*BiMap[K, V], error) {
-	if _, ok := b.keyMap[key]; ok {
+	k, v := unique.Make(key), unique.Make(val)
+	if _, ok := b.keyMap[k]; ok {
 		return nil, fmt.Errorf("duplicate key: %v", key)
 	}
-	if _, ok := b.valMap[val]; ok {
+	if _, ok := b.valMap[v]; ok {
 		return nil, fmt.Errorf("duplicate val: %v", val)
 	}
 
-	b.keyMap[key] = val
-	b.valMap[val] = key
+	b.keyMap[k] = v
+	b.valMap[v] = k
+
+	b.keys[k.Value()] = v.Value()
+	b.values[v.Value()] = k.Value()
 
 	return b, nil
 }
 
 func (b *BiMap[K, V]) GetByKey(key K) (V, bool) {
-	val, ok := b.keyMap[key]
-	return val, ok
+	val, ok := b.keyMap[unique.Make(key)]
+	return val.Value(), ok
 }
 
-func (b *BiMap[K, V]) GetByValue(value V) K {
-	return b.valMap[value]
+func (b *BiMap[K, V]) GetByValue(val V) (K, bool) {
+	key, ok := b.valMap[unique.Make(val)]
+	return key.Value(), ok
 }
 
 func (b *BiMap[K, V]) KeyMapping() map[K]V {
-	return b.keyMap
+	return b.keys
+}
+
+func (b *BiMap[K, V]) ValueMapping() map[V]K {
+	return b.values
 }
