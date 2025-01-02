6f317cbf168738a143646221c5a2f5337bf9bbde
ksCaesar <x246libra@hotmail.com>
2025-01-02 11:48:29 +0800

    refactor: key builder

--- pkg/utility/key_builder.go
+++ pkg/utility/key_builder.go
@@ -5,43 +5,59 @@ import (
 	"sync"
 )
 
-func NewKeyBuilder(prefix string) *KeyBuilder {
-	return &KeyBuilder{
-		pool: sync.Pool{
+func NewKeyBuilder() KeyBuilder {
+	return KeyBuilder{
+		pool: &sync.Pool{
 			New: func() interface{} {
 				return &strings.Builder{}
 			},
 		},
-		prefix: prefix,
 	}
 }
 
 type KeyBuilder struct {
-	pool   sync.Pool
-	prefix string
+	pool *sync.Pool
+	buf  *strings.Builder
 }
 
-func (k *KeyBuilder) get() *strings.Builder {
-	buf := k.pool.Get().(*strings.Builder)
-	if k.prefix != "" {
-		buf.WriteString(k.prefix)
+func (k KeyBuilder) get() *strings.Builder {
+	return k.pool.Get().(*strings.Builder)
+}
+
+func (k KeyBuilder) put() {
+	k.buf.Reset()
+	k.pool.Put(k.buf)
+	k.buf = nil
+}
+
+func (k KeyBuilder) InitWithVersion(version string) KeyBuilder {
+	if version == "" {
+		return k.Init()
 	}
-	return buf
-}
 
-func (k *KeyBuilder) put(buf *strings.Builder) {
-	buf.Reset()
-	k.pool.Put(buf)
-}
-
-func (k *KeyBuilder) BuildString(elements ...string) string {
 	buf := k.get()
-	defer k.put(buf)
+	buf.WriteString(version)
+	return KeyBuilder{
+		pool: k.pool,
+		buf:  buf,
+	}
+}
+
+func (k KeyBuilder) Init() KeyBuilder {
+	buf := k.get()
+	return KeyBuilder{
+		pool: k.pool,
+		buf:  buf,
+	}
+}
+
+func (k KeyBuilder) BuildString(elements ...string) string {
+	defer k.put()
 	for _, element := range elements {
 		if element == "" {
 			element = "empty"
 		}
-		buf.WriteString(element)
+		k.buf.WriteString(element)
 	}
-	return buf.String()
+	return k.buf.String()
 }
